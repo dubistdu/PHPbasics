0dbe23f06340241358ba8aa24e09eac15da95887
Hyun Frantz <hyunfrantz@gmail.com>
2017-06-20 18:17:29 -0400

    php string

---
+++ strings.php
@@ -0,0 +1,10 @@
+<?php
+$name="Jasmine";
+$string_one = "Hello \n$name! \n";
+$string_two = 'Hello $name!';
+$string_three="\nLearning to display \"Hello $name\"";
+echo $string_one;
+echo $string_two;
+echo $string_three;
+
+?>
